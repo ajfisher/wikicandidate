66de23a21d656bd7856099f446975da6390605e1
ajfisher <ajfisher.td@gmail.com>
2016-05-19 23:03:07 +1000

    Added threading support to go faster

--- getdata.py
+++ getdata.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
 import csv
+import Queue
 import requests
 import sys
+from threading import Thread
+
+num_of_threads = 50
 
 senate = "australian_federal_election_2016_-_senate_candidates.txt"
 reps = "australian_federal_election_2016_-_house_of_reps_candidates.txt"
@@ -36,27 +40,70 @@ additional_queries = [
         "incident",
         ]
 
+queuelist = Queue.Queue()
+
+def worker():
+    """
+    goes and does the actual work of getting data
+    """
+    while True:
+        item = queuelist.get()
+        data = get_query_data(item)
+        cw.writerow(data)
+        queuelist.task_done()
+
+def get_query_data(candidate):
+    """
+    Takes a candidate's name and does a query on their data
+    """
+
+    candidate_data = [candidate]
+    zerototal = False
+
+    for query in additional_queries:
+
+        if zerototal is False:
+            data = query if query != "total" else ""
+            payload = {
+                    'q': "%s+%s" % (candidate, data)
+            }
+
+            response = requests.get(wikiurl, params=payload)
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+            count = int(soup.select("div.total-count span")[0].string.replace(',',''))
+        else:
+            count = 0
+
+        candidate_data.append(count)
+
+        # we use this so we don't look up additional queries if it isn't
+        # needed, we just add a bunch of zeros in there.
+        if (query=="total" and count == 0):
+            zerototal = True
+
+    return (candidate_data)
+
+# Main section here kicks everything off
+
 cw = csv.writer(sys.stdout)
 cw.writerow(["name"] + additional_queries)
 
 f = open(file_to_use, 'r')
 
+# open a stack of threads to try and make this go fast
+for i in range(num_of_threads):
+    t = Thread(target=worker)
+    t.daemon = True
+    t.start()
+
+# add the candidates to the queue
 for line in f:
     candidate = line.rstrip()
-    candidate_data = [candidate]
-    for query in additional_queries:
+    queuelist.put(candidate)
 
-        data = query if query != "total" else ""
-        payload = {
-                'q': "%s+%s" % (candidate, data)
-        }
+f.close()
 
-        response = requests.get(wikiurl, params=payload)
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        count = int(soup.select("div.total-count span")[0].string.replace(',',''))
-
-        candidate_data.append(count)
-
-    cw.writerow(candidate_data)
+# wait until all the jobs are complete
+queuelist.join()
 
